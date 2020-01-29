744980dcc04781cd47760b06376aca004b807910
kbs4674 <kbs4674@naver.com>
2020-01-29 18:14:34 +0000

    자동삭제 job : hit_products 테이블 조인 처리

--- app/jobs/auto_delete_job.rb
+++ app/jobs/auto_delete_job.rb
@@ -3,21 +3,31 @@
 
 class AutoDeleteJob < ApplicationJob
 	
-  def hit_product_auto_delete
-		HitProduct.where('created_at < ?', 30.days.ago).each do |x|
+	# def hit_product_auto_delete
+	# 	HitProduct.where('created_at < ?', 30.days.ago).each do |x|
+	# 		x.destroy
+	# 	end
+	# end
+	def hit_product_auto_delete
+		# SELECT A.id FROM hit_products A
+		# 	LEFT OUTER JOIN book_marks B ON A.id = B.hit_product_id
+		# 	LEFT OUTER JOIN keyword_pushalarm_lists C ON A.id = C.hit_product_id
+		# WHERE B.id is NULL AND C.id is NULL;
+		
+		HitProduct.left_joins(:book_marks).left_joins(:keyword_pushalarm_lists).where("book_marks.id is NULL AND keyword_pushalarm_lists.id is NULL").where('hit_products.created_at < ?', 30.days.ago).each do |x|
 		  x.destroy
 		end
 	end
 	
-  def bookmark_auto_delete
+	def bookmark_auto_delete
 		BookMark.where('created_at < ?', 60.days.ago).each do |x|
-		  x.destroy
+			x.destroy
 		end
 	end
 	
-  def pushalarm_list_auto_delete
-		KeywordPushalarmList.where('created_at < ?', 30.days.ago).each do |x|
-		  x.destroy
+	def pushalarm_list_auto_delete
+		KeywordPushalarmList.where('created_at < ?', 60.days.ago).each do |x|
+			x.destroy
 		end
 	end
 	
